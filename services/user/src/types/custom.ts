5da51fc8e329c978cf1964b5133dd63936c7e0c6
Samuel Lim <samuelim01@gmail.com>
2024-10-08 17:45:43 +0800

    User: Remove extra fields

--- services/user/src/types/custom.ts
+++ services/user/src/types/custom.ts
@@ -3,8 +3,6 @@ import { Types } from 'mongoose';
 export interface RequestUser {
     id: Types.ObjectId | string;
     username: string;
-    password?: string;
-    createdAt?: Date;
     email: string;
     isAdmin: boolean;
 }
