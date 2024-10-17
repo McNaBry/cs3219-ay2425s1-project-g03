b2187919dcf0beeaa63be29923d77aa5c4eff28f
McNaBry <mcnabry123@gmail.com>
2024-10-17 23:59:34 +0800

    Integrate matching into frontend

--- frontend/src/app/matching/finding-match/finding-match.component.ts
+++ frontend/src/app/matching/finding-match/finding-match.component.ts
@@ -5,6 +5,12 @@ import { ButtonModule } from 'primeng/button';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { ChipModule } from 'primeng/chip';
 import { CommonModule } from '@angular/common';
+import { catchError, delay, filter, map, Observable, of, repeat, Subject, Subscription, switchMap, take, takeUntil, takeWhile, tap, timer, UnsubscriptionError } from 'rxjs';
+import { QuestionService } from '../../../_services/question.service';
+import { TopicResponse } from '../../questions/topic.model';
+import { MessageService } from 'primeng/api';
+import { MatchService } from '../../../_services/match.service';
+import { MatchResponse, MatchStatus } from '../match.model';
 
 @Component({
     selector: 'app-finding-match',
@@ -15,6 +21,7 @@ import { CommonModule } from '@angular/common';
 })
 export class FindingMatchComponent {
     @Input() userCriteria!: UserCriteria;
+    @Input() matchId!: string;
     @Input() isVisible = false;
 
     @Output() dialogClose = new EventEmitter<void>();
@@ -23,7 +30,15 @@ export class FindingMatchComponent {
 
     isFindingMatch = true;
 
+    matchPoll!: Subscription;
+
+    constructor(
+        private matchService: MatchService,
+        private messageService: MessageService
+    ) {}
+
     closeDialog() {
+        this.matchPoll.unsubscribe();
         this.dialogClose.emit();
     }
 
@@ -37,14 +52,50 @@ export class FindingMatchComponent {
         // Possible to handle routing to workspace here.
     }
 
+    stopPolling$ = new EventEmitter();
+
+    requestData() {
+        return this.matchService.retrieveMatchRequest(this.matchId).pipe(
+            tap((response: MatchResponse) => {
+                console.log(response);
+                const status: MatchStatus = response.data.status || MatchStatus.PENDING
+                switch(status) {
+                    case MatchStatus.MATCH_FOUND:
+                        this.onMatchSuccess();
+                        break;
+                    case MatchStatus.TIME_OUT:
+                        this.stopPolling$.next(false);
+                        this.onMatchFailed();
+                        break;
+                    // TODO: Add case for MatchStatus.COLLAB_CREATED
+                }
+            }),
+            catchError(
+                _ => {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: `Something went wrong while matching.`,
+                        life: 3000,
+                    });
+                    this.closeDialog();
+                    return of(null);
+                }
+            )
+        )
+    }
+
+    startPolling(interval: number): Observable<MatchResponse | null> {
+        return timer(0, interval)
+            .pipe(
+                switchMap(_ => this.requestData())
+            );
+    }
+
     onDialogShow() {
-        // Simulate request to API and subsequent success/failure.
-        setTimeout(() => {
-            if (this.isVisible) {
-                // Toggle to simulate different situations.
-                // this.onMatchFailed();
-                this.onMatchSuccess();
-            }
-        }, 3000);
+        this.matchPoll = this.startPolling(5000).pipe(
+            tap(),
+            takeUntil(this.stopPolling$),
+        ).subscribe();
     }
 }
--- frontend/src/app/matching/matching.component.ts
+++ frontend/src/app/matching/matching.component.ts
@@ -14,6 +14,9 @@ import { QuestionService } from '../../_services/question.service';
 import { MessageService } from 'primeng/api';
 import { HAS_NO_QUESTIONS, hasQuestionsValidator } from './_validators/has-questions.validator';
 import { Difficulty } from './user-criteria.model';
+import { ToastModule } from 'primeng/toast';
+import { MatchService } from '../../_services/match.service';
+import { MatchRequest } from './match.model';
 
 @Component({
     selector: 'app-matching',
@@ -22,6 +25,7 @@ import { Difficulty } from './user-criteria.model';
         FindingMatchComponent,
         RetryMatchingComponent,
         ChipModule,
+        ToastModule,
         MultiSelectModule,
         PanelModule,
         DropdownModule,
@@ -43,10 +47,12 @@ export class MatchingComponent implements OnInit {
     isLoadingTopics = true;
     isProcessingMatch = false;
     isMatchFailed = false;
+    matchId!: string;
 
     constructor(
         private messageService: MessageService,
         private questionService: QuestionService,
+        private matchService: MatchService,
     ) {}
 
     matchForm!: FormGroup;
@@ -104,12 +110,19 @@ export class MatchingComponent implements OnInit {
     }
 
     onMatch() {
-        console.log({
-            topic: this.topics,
-            difficulty: this.difficulty,
+        const matchRequest: MatchRequest = {topics: this.topics, difficulty: this.difficulty}
+        console.log(matchRequest);
+        this.matchService.createMatchRequest(matchRequest).subscribe({
+            next: response => {
+                this.matchId = response.data._id;
+            },
+            error: () => {
+                this.onErrorReceive('Failed to create a match. Please try again later.');
+            },
+            complete: () => {
+                this.isProcessingMatch = true;
+            },
         });
-        this.isProcessingMatch = true;
-        // TODO: Add API request to start matching.
     }
 
     onMatchFailed() {
